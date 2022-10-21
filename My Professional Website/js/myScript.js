2de5247c12c21e65eb4ad18dd6f8721b3b4e4e24
ernestomorales2k <94215811+ernestomorales2k@users.noreply.github.com>
2022-10-21 14:25:08 -0700

    Delete My Professional Website directory

--- My Professional Website/js/myScript.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', function() {
-    var currScrollPos2 = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    while (currScrollPos2 > 0) {
-      document.getElementById('siteHeader').style.opacity = 1;
-    }
-  }
-);
